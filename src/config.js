66b45fdc945114aea1ac63b6aa52139dc12329c4
gitrus <gitrus@yandex.ru>
2016-05-30 12:00:12 +0300

    add configuration

--- src/config.js
+++ src/config.js
@@ -8,9 +8,8 @@ nconf.file({
 });
 
 nconf.defaults({
-  secret: '7c5b358bd47b63ea11bc5ab8a147667108836900d6a4bfd42f4cf75c0e4fa5019c275919873a8c471ec82d1815c4ba6ed48886a4038bf6f062fd38364e8a967c',
   backend: {
-    url: 'http://176.112.203.178:9003',
+    url: 'http://0.0.0.0',
     uploadsTempDir: path.join(__dirname, 'uploads-temp'),
   },
 });
